f1fbe8280cf4b0caee7e008d81d22e645bd8c345
Makarenko <demo6112@gmail.com>
2018-03-15 01:56:02 +0200

    Lab3

---
+++ Lab3/Data.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Data
+    {
+        public int N { get; private set; }
+
+        static object locker = new object();
+
+        public Data(int n)
+        {
+            N = n;
+        }
+
+        // ME = (A* SORT(C))*(MA* ME+MD)
+        public Matrix Func1()
+        {
+            var A = Vector.GetRandomVector(N);
+            var C = Vector.GetRandomVector(N);
+            var MA = Matrix.GetRandomMatrix(N);
+            var ME = Matrix.GetRandomMatrix(N);
+            var MD = Matrix.GetRandomMatrix(N);
+
+            return (MA * ME + MD) * (A * C.Sort());
+        }
+
+        // MF = k* MG - h* MK*ML
+        public Matrix Func2()
+        {
+            int k = 3;
+            int h = 2;
+            var MG = Matrix.GetRandomMatrix(N);
+            var MK = Matrix.GetRandomMatrix(N);
+            var ML = Matrix.GetRandomMatrix(N);
+
+            return MG * k - MK * ML * h;
+        }
+
+        // O = (P+R)*(MS*MT)
+        public Vector Func3()
+        {
+            var P = Vector.GetRandomVector(N);
+            var R = Vector.GetRandomVector(N);
+            var MS = Matrix.GetRandomMatrix(N);
+            var MT = Matrix.GetRandomMatrix(N);
+
+            var tempM = new Matrix(N);
+            tempM[0] = P + R;
+
+            return (tempM * (MS * MT))[0];
+        }
+
+        public static void Print(object o)
+        {
+            lock (locker)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// T = sort(O+T)*trans(MR*MS)
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            System.Console.WriteLine ("Enter N:");
+            int N = int.Parse (Console.ReadLine ());
+            System.Console.WriteLine ("Result:");
+            var task = Calculate (N);
+
+            task.Wait ();
+
+            Console.ReadKey ();
+        }
+
+        public async static Task Calculate (int N)
+        {
+            var O = Vector.GetVectorOfOne (N);
+            var T = Vector.GetVectorOfOne (N);
+            var MR = Matrix.GetMatrixOfOne (N);
+            var MS = Matrix.GetMatrixOfOne (N);
+
+            var oPlusTtask = SumVectors (O, T);
+            var sortVecotrsTask = SortVecotrs (oPlusTtask);
+            var multiplyMatrixTask = MultiplyMatrix (MR, MS);
+            var transMatrixTask = TransMatrix(multiplyMatrixTask);
+            
+            O = await sortVecotrsTask;
+            MR = await transMatrixTask;
+
+            var Om = Matrix.GetMatrixOfZero(N);
+            
+            Om[0] = O;
+
+            System.Console.WriteLine((Om*MR)[0]);
+        }
+
+        public static Task<Vector> SumVectors (Vector O, Vector T)
+        {
+            return Task.Factory.StartNew (() => O + T);
+        }
+
+        public static Task<Vector> SortVecotrs (Task<Vector> oPlusTtask)
+        {
+            return Task.Factory.StartNew (() => oPlusTtask.Result.Sort ());
+        }
+
+        public static Task<Matrix> MultiplyMatrix (Matrix MR, Matrix MS)
+        {
+            return Task.Factory.StartNew (() => MR * MS);
+        }
+
+        public static Task<Matrix> TransMatrix (Task<Matrix> multMatrixTask)
+        {
+            return Task.Factory.StartNew (() => multMatrixTask.Result.TransMatrix());
+        }
+    }
+}
